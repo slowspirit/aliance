1daab413dd02141387a57fb88fdd4c2104487d29
Aleksei <85442704+slowspirit@users.noreply.github.com>
2022-06-21 20:31:55 +0300

    Delete gulp directory

--- gulp/tasks/cleanDist.js
+++
@@ -1,9 +0,0 @@
-import del from 'del';
-export const cleanDist = () => {
-    return del(app.path.clean)
-}
-
-// import del from 'del';
-// export const reset = () => {
-//     return del(app.path.clean)
-// }
